ef6a04440ec910271737708e8cf306b1934e7c73
EC2 Default User <ec2-user@ip-172-31-79-90.ec2.internal>
2021-10-10 19:53:05 +0000

    4-7

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,8 +4,7 @@ class FavoritesController < ApplicationController
     favorite = current_user.favorites.new(post_image_id: post_image.id)
     favorite.save
     redirect_to post_image_path(post_image)
-      
- 　　 end
+    end
 
   def destroy
         post_image = PostImage.find(params[:post_image_id])
